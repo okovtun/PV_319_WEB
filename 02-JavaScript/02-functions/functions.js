b054af04e243c49706f66017c3a9349c4df088fc
User <User@DESKTOP-QHG18FL>
2025-07-24 19:13:21 +0300

    Date in progress

--- 02-JavaScript/02-functions/functions.js
+++ 02-JavaScript/02-functions/functions.js
@@ -143,8 +143,25 @@ function tickCountdown()
     const DAYS_IN_MONTH = 365.25/12;
     const SECONDS_IN_MONTH = SECONDS_IN_DAY*DAYS_IN_MONTH;
     const SECONDS_IN_YEAR = SECONDS_IN_DAY*365 + SECONDS_IN_HOUR*6;
+
+    ////////////////////////////////////////////////////////////////////////////
     //https://stackoverflow.com/questions/14/difference-between-math-floor-and-math-truncate
     let time_of_day = timestamp % SECONDS_IN_DAY;
+    //Убираем время дня из timestamp:
+    let date = Math.floor(timestamp/SECONDS_IN_DAY);
+    date = date * SECONDS_IN_DAY;
+
+    let str_date = '';
+    let years   = Math.floor(date/SECONDS_IN_YEAR); str_date += `Years:${years},`;
+    if(years>0) date = (date%(years*SECONDS_IN_YEAR));
+    let months = Math.floor(date/SECONDS_IN_MONTH);str_date += `Months:${months},`;
+    if(months>0) date = (date%(months*SECONDS_IN_MONTH));
+    let weeks   = Math.floor(date/SECONDS_IN_WEEK); str_date += `Weeks:${weeks},`;
+    if(weeks>0) date = (date%(weeks*SECONDS_IN_WEEK));
+    let days    = Math.floor(date/SECONDS_IN_DAY);  str_date += `Days:${days},`;
+
+    ////////////////////////////////////////////////////////////////////////////
+
     let hours = Math.floor(time_of_day/SECONDS_IN_HOUR);
     if(hours > 0) time_of_day = (time_of_day%(hours*SECONDS_IN_HOUR));
 
@@ -152,6 +169,7 @@ function tickCountdown()
     if(minutes > 0) time_of_day = (time_of_day%(minutes*SECONDS_IN_MINUTE));
 
     let seconds = time_of_day;
+
     ////////////////////////////////////////////////////////////////////////////
 
     document.getElementById("hours-unit").innerHTML = addLeadingZero(hours);
