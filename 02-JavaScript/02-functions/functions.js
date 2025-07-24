ed5285611cd26ed2239b7dd531fb33ac2f43eb60
User <User@DESKTOP-QHG18FL>
2025-07-24 22:10:17 +0300

    Years block DONE

--- 02-JavaScript/02-functions/functions.js
+++ 02-JavaScript/02-functions/functions.js
@@ -152,7 +152,24 @@ function tickCountdown()
 
     let str_date = '';
     let years   = Math.trunc(date/SECONDS_IN_YEAR); str_date += `Years:${years},`;
-    date = date - years*SECONDS_IN_YEAR;
+    if(years != 0)
+    {
+        date = date - years*SECONDS_IN_YEAR;
+        let years_unit = document.getElementById("years-unit");
+        if(years_unit == null)
+        {
+            let display = document.getElementById("display");
+            display.prepend(createTimeBlock("years", years));
+        }
+        else
+        {
+            years_unit.innerHTML = years;
+        }
+    }
+    else
+    {
+        removeTimeBlock("years");
+    }
     //if(years>0) date = (date%(years*SECONDS_IN_YEAR));
     let months = Math.trunc(date/SECONDS_IN_MONTH);str_date += `Months:${months},`;
     date = date - months*SECONDS_IN_MONTH;
@@ -186,4 +203,39 @@ function tickCountdown()
     ////////////////////////////////////////////////////////////////////////////
 
     setTimeout(tickCountdown, 100);
+}
+
+function createTimeBlock(name, value)
+{
+    let time_block = document.createElement("div");
+    time_block.className = "time-block";
+
+    let unit = document.createElement("div");
+    unit.id = `${name}-unit`;
+    unit.className = "time-unit";
+    unit.innerHTML = addLeadingZero(value);
+
+    let marker = document.createElement("div");
+    marker.id = `${name}-marker`;
+    marker.className = "time-marker";
+    marker.innerHTML = name;
+
+    //'String';
+    //"String";
+    //``; - Интерполяция строк
+
+    time_block.append(unit);
+    time_block.append(marker);
+
+    return time_block;
+}
+function removeTimeBlock(name)
+{
+    let unit = document.getElementById(`${name}-unit`);
+    if(unit != null)
+    {
+        let block = unit.parentElement;
+        let block_parent = block.parentElement;
+        block_parent.removeChild(block);
+    }
 }
