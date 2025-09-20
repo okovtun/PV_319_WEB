6bb12d42f8746ad3cfc372ae0f0434f5e9d351a8
User <User@DESKTOP-QHG18FL>
2025-09-20 21:28:14 +0300

    Movies DONE

--- Blazor/Movies/Models/Movie.cs
+++ Blazor/Movies/Models/Movie.cs
@@ -6,10 +6,24 @@ namespace Movies.Models
 	public class Movie
 	{
 		public int Id { get; set; }
+		////////////////////////////////////////////////////////////
+		
+		[Required]
+		[StringLength(60, MinimumLength = 3)]
 		public string? Title { get; set; }
-		public DateOnly ReleaseDate { get; set; }
-		public string? Genre { get; set; }
+		////////////////////////////////////////////////////////////
 
+		[RangeAttribute(typeof(DateOnly), "1888-10-14", "2032-12-31")]
+		public DateOnly ReleaseDate { get; set; }
+		////////////////////////////////////////////////////////////
+
+		[Required]
+		[StringLength(30)]
+		[RegularExpression(@"^[A-Z]+[a-zA-Z()\s-]*$")]
+		public string? Genre { get; set; }
+		////////////////////////////////////////////////////////////
+
+		[Range(0,200)]
 		[DataType(DataType.Currency)]
 		[Column(TypeName = "decimal(18,2)")]
 		public decimal Price { get; set; }
